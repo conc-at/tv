9661e4533e1cfe50f120eecb71a1f6b0122d08be
Hannes Moser <box@hannesmoser.at>
2020-05-24 20:11:42 -0400

    chore: types

---
+++ app/ui/types/4_tagged_union_types.ts
@@ -0,0 +1,27 @@
+// Tagged union types
+// 1. Type the argument and return type of the `dispatch` function.
+
+type CreatePlaylist = {
+  type: 'create';
+  name: string;
+}
+
+type DeletePlaylist = {
+  type: 'delete';
+}
+
+type UpdatePlaylist = {
+  type: 'update';
+  name: string;
+}
+
+function dispatch(action) {
+  switch (action.type) {
+    case 'create':
+      return 'create new playlist'
+    case 'update':
+      return 'update playlist'
+    case 'delete':
+      return 'delete playlist'
+  }
+}
