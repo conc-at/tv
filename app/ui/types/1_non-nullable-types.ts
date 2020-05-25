25076aea9a13c7f6e3865bb52ac9628494cfcc8d
Hannes Moser <box@hannesmoser.at>
2020-05-24 20:11:42 -0400

    chore: types

---
+++ app/ui/types/1_non-nullable-types.ts
@@ -0,0 +1,10 @@
+// Non-nullable types
+// 1. Make variables of type string, number and boolean and make sure they are non nullable
+let name1;
+let count1;
+let active1;
+
+// 2. Make variables of type string, number and boolean and nullable
+let name2;
+let count2;
+let active2;
---
+++ app/ui/types/5_generics.ts
@@ -0,0 +1,32 @@
+// Generics
+// 1. Make this set implementation type safe, but allow any possible type.
+// Don't use `any` to achieve this. The user should be able to provide the type,
+// either through inference or directly but can expect the same type coming back
+// from any of the following functions (except `map`, because it does not make sense).
+//
+// Example:
+//
+// const MySet = new BetterSet([1, 2, 3]);
+// // returns a set of numbers
+// MySet.difference(…)
+//
+
+class BetterSet extends Set {
+  difference(b: BetterSet) {
+    return new BetterSet([...this].filter((a) => !b.has(a)));
+  }
+
+  intersection(b: BetterSet) {
+    return new BetterSet([...this].filter((a) => b.has(a)));
+  }
+
+  union(b: BetterSet) {
+    return new BetterSet([...this, ...b]);
+  }
+
+  map(cb) {
+    for (const a of this) {
+      cb(a);
+    }
+  }
+}
