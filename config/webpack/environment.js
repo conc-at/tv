eb2ed104126ff3e2cd703924fab2dff04492dd30
Hannes Moser <box@hannesmoser.at>
2020-05-24 20:40:18 -0400

    chore: query

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,6 +5,14 @@ const aliasConfig = require('./custom/alias');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const optimizationConfig = require('./custom/optimization');
 
+// Issue: https://github.com/rails/rails/issues/35501
+const nodeModulesLoader = environment.loaders.get('nodeModules');
+if (!Array.isArray(nodeModulesLoader.exclude)) {
+  nodeModulesLoader.exclude =
+    nodeModulesLoader.exclude == null ? [] : [nodeModulesLoader.exclude];
+}
+nodeModulesLoader.exclude.push(/actioncable/);
+
 environment.config.merge(aliasConfig);
 environment.splitChunks(() => optimizationConfig);
 
