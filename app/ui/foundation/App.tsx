e25ac8875d15ea213949affe2e5012f748611736
robinfellinger <rfellinger.mmt-m2020@fh-salzburg.ac.at>
2021-06-01 15:30:24 +0200

    + typescript tasks

--- app/ui/foundation/App.tsx
+++ app/ui/foundation/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import 'sanitize.css';
 import 'sanitize.css/forms.css';
