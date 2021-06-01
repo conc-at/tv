8c3c990c61dceb30e9e1becaa13be18f8dfa579b
Stefan Eisl <seisl.mmt-m2020@fh-salzburg.ac.at>
2021-06-01 15:35:32 +0200

    first part of the exercise

--- app/ui/types/4_tagged_union_types.ts
+++ app/ui/types/4_tagged_union_types.ts
@@ -1,27 +1,29 @@
 // Tagged union types
 // 1. Type the argument and return type of the `dispatch` function.
 
+type ActionMethod = CreatePlaylist | DeletePlaylist | UpdatePlaylist;
+
 type CreatePlaylist = {
   type: 'create';
   name: string;
-}
+};
 
 type DeletePlaylist = {
   type: 'delete';
-}
+};
 
 type UpdatePlaylist = {
   type: 'update';
   name: string;
-}
+};
 
-function dispatch(action) {
+function dispatch(action: ActionMethod) {
   switch (action.type) {
     case 'create':
-      return 'create new playlist'
+      return 'create new playlist';
     case 'update':
-      return 'update playlist'
+      return 'update playlist';
     case 'delete':
-      return 'delete playlist'
+      return 'delete playlist';
   }
 }
