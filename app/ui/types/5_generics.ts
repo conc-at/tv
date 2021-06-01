3bfcb057fbbcb5ccbac3bed2dfa08ba763c31bd9
Luca Pircher <lpircher.mmt-b2017@fh-salzburg.ac.at>
2021-06-01 15:42:15 +0200

    feat(types exercise): complete exercise

--- app/ui/types/5_generics.ts
+++ app/ui/types/5_generics.ts
@@ -11,20 +11,20 @@
 // MySet.difference(…)
 //
 
-class BetterSet extends Set {
-  difference(b: BetterSet) {
+class BetterSet<T> extends Set<T> {
+  difference(b: BetterSet<T>) {
     return new BetterSet([...this].filter((a) => !b.has(a)));
   }
 
-  intersection(b: BetterSet) {
+  intersection(b: BetterSet<T>) {
     return new BetterSet([...this].filter((a) => b.has(a)));
   }
 
-  union(b: BetterSet) {
+  union(b: BetterSet<T>) {
     return new BetterSet([...this, ...b]);
   }
 
-  map(cb) {
+  map(cb: (arg0: T) => void) {
     for (const a of this) {
       cb(a);
     }
