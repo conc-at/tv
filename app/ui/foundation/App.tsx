785ff1c4c475feb0e33f93cf8fcbe6693039c3c9
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-06-02 12:33:39 +0200

    Add(Routing): Route to Home

--- app/ui/foundation/App.tsx
+++ app/ui/foundation/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import 'sanitize.css';
 import 'sanitize.css/forms.css';
 import 'sanitize.css/typography.css';
 import 'react-virtualized/styles.css';
 
-import { Nav } from './components';
+import { Nav } from './components/nav/Nav';
 import './styles.scss';
 import './i18n';
+import { Home } from '../sections/home/Home';
 
 export function App() {
   return (
@@ -20,6 +26,11 @@ export function App() {
       </Helmet>
       <Router>
         <Nav />
+        <Switch>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+        </Switch>
       </Router>
     </>
   );
