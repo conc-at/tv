f7af5eca8c3d3a5ac2a4d27fbb8e3e967f7fa819
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-06-02 16:59:02 +0200

    Adapt: seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
-user = User.create!(name: "anonymous")
-playlist = Playlist.create!(name: "Your playlist", user: user)
+user = User.create!(name: 'Daniela')
+# user = User.find_by(name: "Daniela")
+
+(1..10).each do |index|
+  Playlist.create!(name: "Playlist #{index}", user: user)
+end
