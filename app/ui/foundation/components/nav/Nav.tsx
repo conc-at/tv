e5d6459c6a6b09c065200235f707d3b4212dc831
Hannes Moser <hannes.moser@shopify.com>
2021-06-02 10:36:04 +0200

    chore: fix linter issue

--- app/ui/foundation/components/nav/Nav.tsx
+++ app/ui/foundation/components/nav/Nav.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 
-import { useTranslation } from 'react-i18next';
-
 import { Bar, Item, Section } from './components';
 
 export function Nav() {
-  const { t } = useTranslation();
-
   return (
     <Bar>
       <Section>
