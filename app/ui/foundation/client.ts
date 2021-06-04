9a6354ef668c58cd4430a273a7fdc2a899d13ac2
Daniela Kubesch <daniela.kubesch@outlook.com>
2021-06-04 22:29:04 +0200

    Create and add Apollo Client

---
+++ app/ui/foundation/client.ts
@@ -0,0 +1,9 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:5000/graphql',
+});
+const client = new ApolloClient({ cache, link });
+
+export default client;
